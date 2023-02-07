d668d39ae1fffc9adacc04efa84067892ebaf74a
Thanhtinh06 <nguyenthithanhtinh06@gmail.com>
2023-02-07 13:06:25 +0700

    fix get id task

--- js/models/task.js
+++ js/models/task.js
@@ -3,26 +3,27 @@ class Task {
    * 
    * @param {*} content value of tag input
    * @param {*} status defaul is todo
-   * @property {*} id: get function Date.now()
+   * @property {*} id: get function random ID
    */
   constructor(content,status=STATUS.todo){
     this.content = content;
     this.status = status;
-    this.id = Date.now();
-    //to reload page create new value 
-    location.reload();
-    // this.id = this.getRanDomID();
+    this.id = this.getRanDomID();
   }
+  getRanDomID(){
+   let id = Math.ceil(Math.random()* (MIN_MAX.max- MIN_MAX.min + 1) + MIN_MAX.min);
+   while (id in VALUE_ID){
+    id = Math.ceil(Math.random()* (MIN_MAX.max- MIN_MAX.min + 1) + MIN_MAX.min);
+   }
+   VALUE_ID.push(id);
+   return id;
+  }
+}
 
   /**
-   * Cach 2: Lay tu random
+   * Cach 2: Lay tu Date.now()
    */
-  // getRanDomID(){
-  //  let id = Math.ceil(Math.random()* (MIN_MAX.max- MIN_MAX.min + 1) + MIN_MAX.min);
-  //  while (id in VALUE_ID){
-  //   id = Math.ceil(Math.random()* (MIN_MAX.max- MIN_MAX.min + 1) + MIN_MAX.min);
-  //  }
-  //  VALUE_ID.push(id);
-  //  return id;
-  // }
-}
+
+ // this.id = Date.now();
+    // //to reload page create new value 
+    // location.reload();
